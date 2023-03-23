6229e1a3702aaa148ddd47f8e39df4255a95dc1c
evsyboy <karrapyzzz1@mail.ru>
2023-03-23 20:28:37 +0500

    4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {UiKitModule} from "@evsyboy/ui-kit";
+import {UIInputModule, UIKitModule} from "@evsyboy/ui-kit";
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import {UiKitModule} from "@evsyboy/ui-kit";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UiKitModule
+    UIInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
