266403581572fad2fd6f678af50839295fed535d
evsyboy <karrapyzzz1@mail.ru>
2023-03-23 21:10:23 +0500

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {UIInputModule, UIKitModule} from "@evsyboy/ui-kit";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {UIInputModule, UIKitModule} from "@evsyboy/ui-kit";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UIInputModule
+    UIInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
